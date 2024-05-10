7493810733f0e98e19465cc5358fe9cc01bef26d
Daniele Cortesi <danicortesi.dev@gmail.com>
2024-05-10 22:19:52 +0200

    Update words to replace and replacements

--- src/words.ts
+++ src/words.ts
@@ -1,20 +1,35 @@
 export const wordsToReplace = [
+    "urgentemente",
+    "urgentissimo",
+    "urgentissima",
+    "urgentissimi",
+    "urgentissime",
     "urgente",
+    "urgènte",
     "urgenti",
     "urgenza",
     "urgenze",
+    "urgent",
+    "urgency",
+    "urgens",
+    "urgēre",
+    "urgere",
+    "urge",
 ];
 
 export const replacements = [
     "l'erba è alta",
     "file descriptor virtuale",
     "ROLEX",
+    "io non sono ISO",
+    "ISO 69420",
+    "bello il MePa eh?",
 
     "bbbeeehhh amico mio!!!",
     "non sei biù rock?",
     "non sei biù metal?",
     "a mio disgabido, gabido?",
-    "dai che funziona",
+    "dai che funzionaaaa",
 
     "amica chips",
 
@@ -31,6 +46,7 @@ export const replacements = [
 
     "UrGeNtEeE MMMMMMFFFFFFFFFFFFPPP",
     "Lo vOlEvo iErI GGGGGRRRRRRRRR",
+    "AAAAAAAAAAAAAAAAAAaAAaaAaAaaAAA",
 
     "do you ringo?",
     "interloquiamone",
